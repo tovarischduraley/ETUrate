a1b0e57e64265366c629e3bdf896848202f6e1a9
Dmitry Sidorov <VIWET42@gmail.com>
2021-02-18 14:27:30 +0300

    Modified .gitignore

--- RT/settings.py
+++ RT/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '5432',
     }
