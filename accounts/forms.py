28555b5a91d8073358f37b04da31b76097593c6f
Vladimir Stepanov <vovastepanov777@gmail.com>
2021-03-03 18:58:33 +0300

    Finished registration form

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,6 +4,13 @@ from .models import Profile
 
 
 class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(required=True, label='Имя')
+    last_name = forms.CharField(required=True, label='Фамилия')
+    patronymic = forms.CharField(required=True, label='Отчество')
+    group_number = forms.IntegerField(max_value=9999, required=True, label='Номер группы')
+    password1 = forms.CharField(widget=forms.PasswordInput, label='Пароль')
+    password2 = forms.CharField(widget=forms.PasswordInput, label='Подтвердите пароль')
+
     class Meta:
         model = Profile
-        fields = ["first_name", "last_name", "patronymic", "email", "password1", "password2"]
+        fields = ["first_name", "last_name", "patronymic", "group_number", "email", "password1", "password2"]
