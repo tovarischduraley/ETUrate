b716966397efce6d0d06172d9fd8938c26ceacec
Sidorov Dmitry <VIWET42@yandex.ru>
2021-03-03 18:05:57 +0300

    Added registration view

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from .models import Profile
 
 
 class RegisterForm(UserCreationForm):
     class Meta:
-        model = User
-        fields = ["first_name", "last_name", "username", "password1", "password2"]
+        model = Profile
+        fields = ["first_name", "last_name", "patronymic", "email", "password1", "password2"]
--- accounts/views.py
+++ accounts/views.py
@@ -1,2 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from .forms import RegisterForm
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'accounts/register.html', context={'form': form})
