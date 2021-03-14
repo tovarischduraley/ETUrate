4166760e20493c25018ef8c528deb60cdb54c6e1
Vladimir Stepanov <vovastepanov777@gmail.com>
2021-03-14 23:07:04 +0300

    Finnished password reset system

--- RT/settings.py
+++ RT/settings.py
@@ -138,7 +138,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS =True
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'eturateservice@gmail.com'
+EMAIL_HOST_PASSWORD = 'fmfwsjtwvznouydf'
 
 LOGIN_REDIRECT_URL = 'home'
