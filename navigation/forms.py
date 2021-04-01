f49b8f87cec201f33ede9805d1bbe77c49d8bd28
Sidorov Dmitry <VIWET42@yandex.ru>
2021-04-02 02:08:34 +0300

    Added cathedra_head_only and staff_only decorators.

--- navigation/forms.py
+++ navigation/forms.py
@@ -3,17 +3,30 @@ from django import forms
 from .models import *
 
 
-class FacultyCreationForm(forms.ModelForm):
+class FacultyCreateForm(forms.ModelForm):
     class Meta:
         model = Faculty
         fields = ('title', 'info', 'image',)
 
 
 class TeacherCreateEditForm(forms.ModelForm):
-    courses = forms.ModelMultipleChoiceField(queryset=Course.objects.all(), widget=forms.CheckboxSelectMultiple(),
-                                             label='Курсы')
+    courses = forms.ModelMultipleChoiceField(queryset=Course.objects.all(), label='Курсы')
 
     class Meta:
         model = Teacher
         fields = ('first_name', 'last_name', 'patronymic', 'courses', 'speciality', 'is_lecturer', 'is_practical',
-                  'birth_date', 'avatar')
+                  'birth_date', 'avatar',)
+
+
+class CourseCreationForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = ('title',)
+
+
+class CourseEditForm(forms.ModelForm):
+    teachers = forms.ModelMultipleChoiceField(queryset=Teacher.objects.all(), label='Курсы')
+
+    class Meta:
+        model = Course
+        fields = ('title', 'teachers',)
--- navigation/views.py
+++ navigation/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 
-from .forms import FacultyCreationForm, TeacherCreateEditForm
+from .forms import FacultyCreateForm, TeacherCreateEditForm
 from .models import *
+from .decorators import *
 
 
 def index(request):
@@ -29,69 +30,72 @@ def teacher_detail(request, teacher_id):
     return render(request, 'navigation/teacher_detail.html', context={'teacher': teacher, 'cathedras': cathedras})
 
 
+@cathedra_head_only
 def cathedra_control(request):
-    if request.user.is_cathedra_head:
-        teachers = request.user.cathedra.teachers.all()
-        return render(request, 'navigation/cathedra_control.html', context={'teachers': teachers})
+    teachers = request.user.cathedra.teachers.all()
+    return render(request, 'navigation/cathedra_control.html', context={'teachers': teachers})
 
 
+@cathedra_head_only
 def teacher_create(request):
-    if request.user.is_cathedra_head:
-        if request.method == 'POST':
-            teacher = Teacher.objects.create()
-            teacher.cathedras.add(request.user.cathedra)
-            form = TeacherCreateEditForm(request.POST, request.FILES, instance=teacher)
-            if form.is_valid():
-                for course in form.cleaned_data['courses'].all():
-                    teacher.courses.add(course)
-                form.save()
-                return redirect('cathedra_control_url')
-        else:
-            form = TeacherCreateEditForm()
-        return render(request, 'navigation/teacher_creation.html', context={'form': form})
+    if request.method == 'POST':
+        teacher = Teacher.objects.create()
+        teacher.cathedras.add(request.user.cathedra)
+        form = TeacherCreateEditForm(request.POST, request.FILES, instance=teacher)
+        if form.is_valid():
+            for course in form.cleaned_data['courses'].all():
+                teacher.courses.add(course)
+            form.save()
+            return redirect('cathedra_control_url')
+    else:
+        form = TeacherCreateEditForm()
+    return render(request, 'navigation/teacher_creation.html', context={'form': form})
 
 
+@cathedra_head_only
 def teacher_edit(request, teacher_id=None):
-    if request.user.is_cathedra_head:
-        teacher = get_object_or_404(Teacher, id=teacher_id)
-        if request.method == 'POST':
-            form = TeacherCreateEditForm(request.POST, request.FILES, instance=teacher)
-            if form.is_valid():
-                teacher.courses.through.objects.all().delete()
-                for course in form.cleaned_data['courses'].all():
-                    teacher.courses.add(course)
+    teacher = get_object_or_404(Teacher, id=teacher_id)
+    if request.method == 'POST':
+        form = TeacherCreateEditForm(request.POST, request.FILES, instance=teacher)
+        if form.is_valid():
+            teacher.courses.through.objects.all().delete()
+            for course in form.cleaned_data['courses'].all():
+                teacher.courses.add(course)
                 form.save()
-                return redirect('cathedra_control_url')
-        else:
-            form = TeacherCreateEditForm(instance=teacher, initial={'courses': teacher.courses.all()})
-        return render(request, 'navigation/teacher_edit.html', context={'form': form})
+            return redirect('cathedra_control_url')
+    else:
+        form = TeacherCreateEditForm(instance=teacher, initial={'courses': teacher.courses.all()})
+    return render(request, 'navigation/teacher_edit.html', context={'form': form})
 
 
+@cathedra_head_only
 def teacher_delete(request, teacher_id=None):
-    if request.user.is_cathedra_head:
-        teacher = get_object_or_404(Teacher, id=teacher_id)
-        if request.method == 'POST':
-            teacher.delete()
-            return redirect('cathedra_control_url')
-        else:
-            return redirect('cathedra_control_url')
+    teacher = get_object_or_404(Teacher, id=teacher_id)
+    if request.method == 'POST':
+        teacher.delete()
+        return redirect('cathedra_control_url')
+
+
+@cathedra_head_only
+def course_create(request):
+    pass
 
 
 """Дождаться решения по поводу админской панели"""
 
 
+@staff_only
 def admin_panel(request):
-    if request.user.is_staff:
-        return render(request, 'navigation/admin_panel.html')
+    return render(request, 'navigation/admin_panel.html')
 
 
+@staff_only
 def faculty_create(request):
-    if request.user.is_staff:
-        if request.method == 'POST':
-            form = FacultyCreationForm(request.POST, request.FILES)
-            if form.is_valid():
-                form.save()
-                return redirect('admin_panel_url')
-        else:
-            form = FacultyCreationForm()
-        return render(request, 'navigation/faculty_creation.html', context={'form': form})
+    if request.method == 'POST':
+        form = FacultyCreateForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('admin_panel_url')
+    else:
+        form = FacultyCreateForm()
+    return render(request, 'navigation/faculty_creation.html', context={'form': form})
