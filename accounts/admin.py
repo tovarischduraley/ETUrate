750b217e494eb94cd8ccff9096ef08a8b1c563e2
Sidorov Dmitry <VIWET42@yandex.ru>
2021-04-28 20:00:39 +0300

    Fixed cathedra report bugs

--- accounts/admin.py
+++ accounts/admin.py
@@ -12,14 +12,14 @@ class ProfileAdmin(UserAdmin):
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Персональные данные', {'fields': ('last_name', 'first_name', 'patronymic', 'group_number' )}),
-        ('Права доступа', {'fields': ('is_staff', 'is_student', 'is_cathedra_head', 'cathedra')}),
+        ('Права доступа', {'fields': ('is_staff', 'is_student', 'is_cathedra_head', 'is_active' 'cathedra')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
             'fields': (
             'email', 'password1', 'password2', 'last_name', 'first_name', 'patronymic', 'group_number', 'is_staff',
-            'is_student', 'is_cathedra_head', 'cathedra')}
+            'is_student', 'is_cathedra_head', 'is_active', 'cathedra')}
          ),
     )
 
