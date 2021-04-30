65f3514c500a3edacf7226c00dff606268797bc5
Vladimir Stepanov <vovastepanov777@gmail.com>
2021-04-30 05:01:51 +0300

    Added admin panel forms pages templates

--- admin_panel/forms.py
+++ admin_panel/forms.py
@@ -1,11 +1,19 @@
 from django import forms
-from datetime import date
 
 from accounts.models import Profile
 from navigation.models import *
 
+widget = forms.TextInput(attrs={
+    'class': 'input__text',
+})
+
+ta_widget = forms.Textarea(attrs={
+    'class':'text__area'
+})
 
 class FacultyCreateEditForm(forms.ModelForm):
+    title = forms.CharField(widget=widget, required=True, label='Название факультета')
+    info = forms.CharField(widget=ta_widget, required=True, label='Описание факультета')
     class Meta:
         model = Faculty
         fields = ('title', 'info', 'image',)
@@ -22,16 +30,23 @@ class CourseCreateForm(forms.ModelForm):
 
 
 class CathedraCreateEditForm(forms.ModelForm):
-    info = forms.CharField(widget=forms.Textarea, required=True, label='Описание кафедры')
+    title = forms.CharField(widget=widget, required=True, label='Название кафедры')
+    info = forms.CharField(widget=ta_widget, required=True, label='Описание кафедры')
 
     class Meta:
         model = Cathedra
         fields = ('title', 'info', 'image',)
 
 
+c_widget = forms.Select(attrs={'class': 'select'})
+
+
 class CathedraHeadRegisterForm(forms.ModelForm):
-    cathedra = forms.ModelChoiceField(queryset=Cathedra.objects.all(), required=True, label='Кафедра')
-    patronymic = forms.CharField(widget=forms.TextInput, label='Отчество', required=False)
+    cathedra = forms.ModelChoiceField(widget=c_widget, queryset=Cathedra.objects.all(), required=True, label='Кафедра')
+    patronymic = forms.CharField(widget=widget, label='Отчество', required=False)
+    last_name = forms.CharField(widget=widget, label='Фамилия', required=True)
+    first_name = forms.CharField(widget=widget, label='Имя', required=True)
+    email = forms.EmailField(widget=widget, label='Email', required=True)
 
     class Meta:
         model = Profile
