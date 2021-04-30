68f210a7cd78c5dc0df1aa3c1ef396d31853917c
Sidorov Dmitry <viwet42@yandex.ru>
2021-04-30 13:39:05 +0300

    Add is_active field in DjangoAdmin profile form

--- accounts/admin.py
+++ accounts/admin.py
@@ -8,11 +8,11 @@ class ProfileAdmin(UserAdmin):
     form = ProfileChangeForm
     add_form = ProfileCreationForm
 
-    list_display = ('email', 'last_name', 'first_name', 'patronymic', 'is_staff')
+    list_display = ('email', 'last_name', 'first_name', 'patronymic', 'is_staff', )
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Персональные данные', {'fields': ('last_name', 'first_name', 'patronymic', 'group_number' )}),
-        ('Права доступа', {'fields': ('is_staff', 'is_student', 'is_cathedra_head', 'is_active' 'cathedra')}),
+        ('Права доступа', {'fields': ('is_staff', 'is_student', 'is_cathedra_head', 'is_active', 'cathedra')}),
     )
     add_fieldsets = (
         (None, {
